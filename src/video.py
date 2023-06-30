85fea561615e7d905962df3c9f304e77e89df568
artem <artemshmoylov123@gmail.com>
2023-06-30 18:42:09 +0300

    доблено исключение если указан не верный айди видио

--- src/video.py
+++ src/video.py
@@ -7,16 +7,39 @@ class Video:
     api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, video_id):
-        self.video_response = self.youtube().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                           id=video_id).execute()
         self.__video_id = video_id
-        self.__video_title = self.video_response['items'][0]['snippet']['title']
-        self.__video_url = f'https://www.youtube.com/watch?v={video_id}'
-        self.__video_view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.__video_like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                               id=video_id).execute()
+
+            self.__video_title = self.video_response['items'][0]['snippet']['title']
+            self.__video_url = f'https://www.youtube.com/watch?v={video_id}'
+            self.__video_view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.__video_like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.__video_like_count = None
+            self.__video_view_count = None
+            self.__video_title = None
+            self.__video_url = None
 
     def __str__(self):
         return f"{self.__video_title}"
+
+    @property
+    def like_count(self):
+        return self.__video_like_count
+
+    @property
+    def title(self):
+        return self.__video_title
+
+    @property
+    def video_url(self):
+        return self.__video_url
+
+    @property
+    def view_count(self):
+        return self.__video_view_count
 
     @classmethod
     def youtube(cls):
